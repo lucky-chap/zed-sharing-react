d134a94900d7566b63d333279ea9316c2ff5279b
Quavo Huncho <hunchodotdev@gmail.com>
2022-06-19 08:03:03 +0000

    refactor: :fire: remove redundant stun servers

--- src/domains/domain.dev.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  baseUrl: `${process.env.REACT_APP_DOMAIN_DEV}`,
-};
--- src/domains/domain.prod.ts
+++
@@ -1,4 +0,0 @@
-export const domain = {
-  production: true,
-  baseUrl: `${process.env.REACT_APP_DOMAIN_PROD}`,
-};
